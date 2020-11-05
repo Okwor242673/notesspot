21313c7273a08eb6e575fb012603c43196e827b1
Okwor242673 <67395013+Okwor242673@users.noreply.github.com>
2020-11-05 18:23:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
-const navSlide = () =>{
-	const burger = document.querySelector('.burger');
-	const nav =document.querySelector('.nav-links');
-	const navLinks=document.querySelectorAll('.nav-links li')
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
 
+});
 
-	burger.addEventListener('click',()=>{
-		nav.classList.toggle('nav-active');
-			//animate links
-		navLinks.forEach((link,index) => {
-      if(link.style.animation){
-      	link.style.animation='';
-      }else{
-      	link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.5}s`;
-       }
-	 });
-
-		//burger animation
-		burger.classList.toggle('toggle');
-	});
-
-
-}
-navSlide();
+$(window).scroll(function() {
+            if ($(document).scrollTop() > 50) {
+                $('.nav').addClass('affix');
+                console.log("OK");
+            } else {
+                $('.nav').removeClass('affix');
+            }
+        });
